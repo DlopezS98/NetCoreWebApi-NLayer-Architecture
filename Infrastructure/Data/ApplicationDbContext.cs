52e55898ebbdc9cb78151ffd1d3b95ae247b05b6
DlopezS98 <01dlopezs98@gmail.com>
2023-08-27 16:23:57 -0600

    Database connection setup - ApplicationDbContext

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<ToDo> ToDo { get; set; }
+}
--- Infrastructure/InfrastructureConfigureServices.cs
+++ Infrastructure/InfrastructureConfigureServices.cs
@@ -1,13 +1,21 @@
-﻿using Infrastructure.Data.Interfaces;
+﻿using Infrastructure.Data;
+using Infrastructure.Data.Interfaces;
 using Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure;
+
 public static class InfrastructureConfigureServices
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options => 
+            options.UseSqlServer(configuration.GetConnectionString("MSSQLServer")),
+            ServiceLifetime.Singleton
+        );
+
         services.AddScoped<IToDoRespository, ToDosRepository>();
         return services;
     }
